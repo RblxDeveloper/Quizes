e5f49905a55d2dde8587870e0327c781f5b280e5
ProScripter <110280452+RblxDeveloper@users.noreply.github.com>
2022-11-15 16:50:20 +0300

    Add files via upload

---
+++ question.js
@@ -0,0 +1,259 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "I ....... 15 years old.",
+  answer: "am",
+  options: [
+    "were",
+    "don't",
+    "am",
+    "went"
+  ]
+},
+  {
+  numb: 2,
+  question: "Do you eat insects?",
+  answer: "No, i don't",
+  options: [
+    "yes",
+    "no",
+    "Yes, i do",
+    "No, i don't"
+  ]
+},
+{
+  numb: 3,
+  question: "How ....... water did she have?",
+  answer: "much",
+  options: [
+    "many",
+    "long",
+    "much",
+    "often"
+  ]
+},
+{
+  numb: 4,
+  question: "He ....... a junior chess competition when he was 10 years old.",
+  answer: "won",
+  options: [
+    "wins",
+    "won",
+    "win",
+    "to win"
+  ]
+},
+{
+  numb: 5,
+  question: "He took a car by ....... .",
+  answer: "himself",
+  options: [
+    "himself",
+    "myself",
+    "themselves",
+    "ourselves"
+  ]
+},
+{
+  numb: 6,
+  question: "I ....... going to wash my hair tomorrow.",
+  answer: "am",
+  options: [
+    "will",
+    "are",
+    "am",
+    "do"
+  ]
+},
+{
+  numb: 7,
+  question: "I buy things ....... myself.",
+  answer: "by",
+  options: [
+    "as",
+    "by",
+    "at",
+    "with"
+  ]
+},
+{
+  numb: 8,
+  question: "I’m going to the ....... as I have a toothache.",
+  answer: "dentist",
+  options: [
+    "farmer",
+    "dentist",
+    "grocer",
+    "baker"
+  ]
+},
+{
+  numb: 9,
+  question: "The camera is ......., it costs a lot of money.",
+  answer: "expensive",
+  options: [
+    "expensive",
+    "high",
+    "cheap",
+    "good"
+  ]
+},
+{
+  numb: 10,
+  question: "What ....... you do yesterday?",
+  answer: "did",
+  options: [
+    "have",
+    "did",
+    "will",
+    "are"
+  ]
+},
+{
+  numb: 11,
+  question: "It ....... delicious.",
+  answer: "looks",
+  options: [
+    "to look",
+    "looking",
+    "look",
+    "looks"
+  ]
+},
+{
+  numb: 12,
+  question: "This is my house. I’m the ....... of it.",
+  answer: "owner",
+  options: [
+    "king",
+    "friend",
+    "owner",
+    "chef"
+  ]
+},
+{
+  numb: 13,
+  question: "I like ....... soup.",
+  answer: "chicken",
+  options: [
+    "chicken",
+    "lemonade",
+    "apple",
+    "fish"
+  ]
+},
+{
+  numb: 14,
+  question: "Where did the first chips factory ....... ?",
+  answer: "open",
+  options: [
+    "opens",
+    "opening",
+    "opened",
+    "open"
+  ]
+},
+{
+  numb: 15,
+  question: "Did she ....... the fish?",
+  answer: "eat",
+  options: [
+    "eat",
+    "taken",
+    "eaten",
+    "eat to"
+  ]
+},
+{
+  numb: 16,
+  question: "He ....... Swimming in the pool.",
+  answer: "likes",
+  options: [
+    "like",
+    "does",
+    "likes",
+    "will"
+  ]
+},
+{
+  numb: 17,
+  question: "Which bus ....... i get?",
+  answer: "should",
+  options: [
+    "were",
+    "should",
+    "have",
+    "when"
+  ]
+},
+{
+  numb: 18,
+  question: "....... was your meal?",
+  answer: "How",
+  options: [
+    "How",
+    "When",
+    "Where",
+    "What"
+  ]
+},
+{
+  numb: 19,
+  question: "....... you play everyday?",
+  answer: "Do",
+  options: [
+    "Did",
+    "Will",
+    "When",
+    "Do"
+  ]
+},
+{
+  numb: 20,
+  question: "When ....... they go to the school?",
+  answer: "did",
+  options: [
+    "have",
+    "did",
+    "do",
+    "were"
+  ]
+},
+{
+  numb: 21,
+  question: "When will ....... cook the food?",
+  answer: "she",
+  options: [
+    "she",
+    "her",
+    "herself",
+    "himself"
+  ]
+},
+{
+  numb: 22,
+  question: "I ....... a lot of homeworks yesterday.",
+  answer: "had",
+  options: [
+    "have",
+    "had",
+    "am",
+    "was"
+  ]
+},
+// you can uncomment the below codes and make duplicate as more as you want to add question
+// but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+//   {
+//   numb: 6,
+//   question: "Your Question is Here",
+//   answer: "Correct answer of the question is here",
+//   options: [
+//     "Option 1",
+//     "option 2",
+//     "option 3",
+//     "option 4"
+//   ]
+// },
+];
